5a006f7febc66ec7a7a376c364c7525b5874e68b
himanoa <matsunoappy@gmail.com>
2022-05-02 15:09:25 +0900

    Add rename command test

--- src/commands/rename.rs
+++ src/commands/rename.rs
@@ -73,3 +73,124 @@ impl RenameCommand for RenameCommandImpl {
             .execute(&scaffolds, identify, replaced_identify)
     }
 }
+
+#[cfg(not(tarpaulin_include))]
+#[cfg(test)]
+mod tests {
+    use crate::commands::rename::{RenameCommand, RenameCommandImpl};
+    use crate::error::MdmgError;
+    use crate::logger::Logger;
+    use crate::rename_executor::RenameExecutor;
+    use crate::template::Template;
+    use crate::template_repository::TemplateRepository;
+    use derive_more::Constructor;
+
+    use std::sync::Arc;
+
+    #[test]
+    fn test_rename_command_run_template_is_not_found() {
+        #[derive(Constructor, Debug)]
+        struct DummyTemplateRepository;
+
+        impl TemplateRepository for DummyTemplateRepository {
+            fn resolve(&self, _template_name: String) -> crate::Result<crate::template::Template> {
+                Err(MdmgError::TemplateIsNotFound("dummy".to_string()))
+            }
+            fn list(&self) -> crate::Result<Vec<crate::file::FileName>> {
+                Ok(vec![])
+            }
+        }
+
+        #[derive(Constructor, Debug)]
+        struct DummyLogger;
+
+        impl Logger for DummyLogger {
+            fn info(&self, _log: &str) {
+                ()
+            }
+            fn debug(&self, _log: &str) {
+                ()
+            }
+        }
+
+        #[derive(Debug, Constructor)]
+        struct DummyRenameExecutor;
+
+        impl RenameExecutor for DummyRenameExecutor {
+            fn execute(
+                &self,
+                _scaffolds: &[crate::scaffold::Scaffold],
+                _before_identify: &str,
+                _after_identify: &str,
+            ) -> crate::Result<()> {
+                Ok(())
+            }
+        }
+
+        let command = RenameCommandImpl {
+            template_repository_instance: Arc::new(DummyTemplateRepository::new()),
+            logger_instance: Arc::new(DummyLogger::new()),
+            rename_executor_instance: Arc::new(DummyRenameExecutor::new()),
+        };
+
+        let result = command.run("dummy", "dummy", "dummy");
+        assert!(result.is_err())
+    }
+
+    #[test]
+    fn test_rename_command_run_template_is_ok() {
+        #[derive(Constructor, Debug)]
+        struct DummyTemplateRepository;
+
+        impl TemplateRepository for DummyTemplateRepository {
+            fn resolve(&self, _template_name: String) -> crate::Result<crate::template::Template> {
+                Ok(Template::new("aaa"))
+            }
+            fn list(&self) -> crate::Result<Vec<crate::file::FileName>> {
+                Ok(vec![])
+            }
+        }
+
+        #[derive(Constructor, Debug)]
+        struct DummyLogger;
+
+        impl Logger for DummyLogger {
+            fn info(&self, _log: &str) {
+                ()
+            }
+            fn debug(&self, _log: &str) {
+                ()
+            }
+        }
+
+        #[derive(Debug, Constructor)]
+        struct DummyRenameExecutor;
+
+        impl RenameExecutor for DummyRenameExecutor {
+            fn execute(
+                &self,
+                _scaffolds: &[crate::scaffold::Scaffold],
+                _before_identify: &str,
+                _after_identify: &str,
+            ) -> crate::Result<()> {
+                Ok(())
+            }
+        }
+
+        let command = RenameCommandImpl {
+            template_repository_instance: Arc::new(DummyTemplateRepository::new()),
+            logger_instance: Arc::new(DummyLogger::new()),
+            rename_executor_instance: Arc::new(DummyRenameExecutor::new()),
+        };
+
+        let result = command.run("dummy", "dummy", "dummy");
+        assert!(result.is_ok())
+    }
+
+    #[test]
+    #[cfg_attr(not(feature = "fs-test"), ignore)]
+    fn test_rename_command_impl_new_test() {
+        RenameCommandImpl::new();
+        assert!(true)
+    }
+}
